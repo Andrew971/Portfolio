a2acc41d67bb8407d298556e37a79d62bd0b44ec
Andrew971 <sordier.andrew@gmail.com>
2018-05-17 15:26:47 -0700

    update

--- client/src/Components/ModalContent.js
+++ client/src/Components/ModalContent.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 export default styled.div`
 background-color: #fefefe;
 margin: 10vh auto; /* 10% from the top and centered */
-padding: 1rem;
+padding: 2rem;
 border: 1px solid #888;
-width: 80%; /* Could be more or less, depending on screen size */
+width: 100%; /* Could be more or less, depending on screen size */
 
 @media (min-width: 660px) {
-  padding: 5rem;
+  padding: 2rem 5rem;
+  width: 80%; /* Could be more or less, depending on screen size */
 
 }
   `;
